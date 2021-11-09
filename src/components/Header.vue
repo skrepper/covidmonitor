25ff63b2915d1b23458d9b50ad43f4c63f0cf268
Dilgerov Tsiden <skrepper@yandex.ru>
2021-11-09 12:40:11 +0300

    tailwind update

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="text-center bg-blue-600 text-white p-4 mb-8">
+        <div class="text-4xl md:text-5xl font-bold mb-3">
+            <i class="fa fa-viruses"></i>Covid-19 Tracker
+        </div>
+        <p class="">
+            API by
+            <a class="text-blue-300" href="https://covid19api.com/" target="_blank">covid19api.com</a>
+        </p>
+    </header>
+</template>
+
+
+<script>
+    export default {
+        name: "Header",
+    };
+</script>
