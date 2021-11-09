c3496716084cf851536af34f9cbf13f12e0c2d71
Dilgerov Tsiden <skrepper@yandex.ru>
2021-11-09 12:50:36 +0300

    color font css

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,11 @@
 <template>
-    <header class="text-center bg-blue-600 text-white p-4 mb-8">
-        <div class="text-4xl md:text-5xl font-bold mb-3">
+    <header class="text-left bg-blue-200 text-white p-10 mb-2">
+        <div class="text-8xl md:text-15xl font-bold mb-3">
             <i class="fa fa-viruses"></i>Covid-19 Tracker
         </div>
-        <p class="">
+        <p class="text-red-300">
             API by
-            <a class="text-blue-300" href="https://covid19api.com/" target="_blank">covid19api.com</a>
+            <a class="text-red-400 font-bold" href="https://covid19api.com/" target="_blank">covid19api.com</a>
         </p>
     </header>
 </template>
